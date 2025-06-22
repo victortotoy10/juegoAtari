7f4697b57b3147045c62d9a1e12c985f0c6281c9
Victor Totoy <totoyvictor10@gmail.com>
2025-06-21 19:20:10 -0500

    controles para el jugador

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var DIRECTION = {
 // Configuración del juego
 var rounds = [5, 5, 3, 3, 2];
 var colors = ['#1abc9c', '#2ecc71', '#3498db', '#e74c3c', '#9b59b6'];
-
+ 
 // Objeto pelota
 var Ball = {
   new: function (speed) {
@@ -40,3 +40,45 @@ var Paddle = {
     };
   }
 };
+var Game = {
+  initialize: function () {
+    this.canvas = document.querySelector('canvas');
+    this.context = this.canvas.getContext('2d');
+
+    this.canvas.width = 1000;
+    this.canvas.height = 700;
+
+    this.player = Paddle.new.call(this, 'left');
+    this.paddle = Paddle.new.call(this, 'right');
+    this.ball = Ball.new.call(this);
+
+    this.running = this.over = false;
+    this.turn = this.paddle;
+    this.timer = this.round = 0;
+    this.color = '#2c3e50';
+
+    Pong.listen();
+  },
+
+  listen: function () {
+    document.addEventListener('keydown', function (key) {
+      // Empezar el juego al presionar cualquier tecla
+      if (Pong.running === false) {
+        Pong.running = true;
+        window.requestAnimationFrame(Pong.loop);
+      }
+
+      // Teclas para mover el jugador
+      if (key.keyCode === 38 || key.keyCode === 87) Pong.player.move = DIRECTION.UP;
+      if (key.keyCode === 40 || key.keyCode === 83) Pong.player.move = DIRECTION.DOWN;
+    });
+
+    document.addEventListener('keyup', function () {
+      Pong.player.move = DIRECTION.IDLE;
+    });
+  }
+};
+
+var Pong = Object.assign({}, Game);
+Pong.initialize();
+
